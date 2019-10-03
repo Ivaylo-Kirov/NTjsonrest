66244e72178cf766f4c0611baa9bcd7422ebd938
Ivaylo-Kirov <ivo.kirov@gmail.com>
2019-10-03 19:32:08 -0400

    Added async/await method

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ axios.get('https://hlv7793ve8.execute-api.us-east-1.amazonaws.com/jsontest')
     })
     .catch((err) => console.log(err));
 
+// async / await approach
+async function AWSTest() {
+    try {
+        const response = await axios.get('https://hlv7793ve8.execute-api.us-east-1.amazonaws.com/jsontest');
+        console.log(response.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+AWSTest();
